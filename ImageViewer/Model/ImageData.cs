a0eaf169ea5d0aac640729d98680d930dad41685
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-04-03 10:27:03 +0900

    first commit

---
+++ ImageViewer/Model/ImageData.cs
@@ -0,0 +1,33 @@
+﻿namespace ImageViewer.Model
+{
+    public class ImageData
+    {
+        public Photos photos { get; set; }
+        public string stat { get; set; }
+    }
+
+    public class Photos
+    {
+        public long page { get; set; }
+        public long pages { get; set; }
+        public long perpage { get; set; }
+        public string total { get; set; }
+        public Photo[] photo { get; set; }
+    }
+
+    public class Photo
+    {
+        public string id { get; set; }
+        public string owner { get; set; }
+        public string secret { get; set; }
+        public string server { get; set; }
+        public long farm { get; set; }
+        public string title { get; set; }
+        public long ispublic { get; set; }
+        public long isfriend { get; set; }
+        public long isfamily { get; set; }
+
+
+        public string Url => $"http://farm{farm}.staticflickr.com/{server}/{id}_{secret}_q.jpg";
+    }
+}
---
+++ ImageViewer/Model/ImageLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace ImageViewer.Model
+{
+    public class ImageLoader
+    {
+        private readonly HttpClient _httpClient;
+
+        public ImageLoader(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<ImageData> LoadAsync(string tags)
+        {
+            var url = MakeRequestUrl(tags);
+
+            return _httpClient.GetJsonAsync<ImageData>(url);
+        }
+
+        private static string MakeRequestUrl(string tags)
+            => $"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f5da5b9adea8918bba00916e81bc1a85&tags={tags}&format=json&nojsoncallback=1";
+    }
+}
